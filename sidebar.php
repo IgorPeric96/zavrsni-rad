f7e72e4f0759ee1c2b4ef665a4c629dfea22a93b
IgorPeric96 <igorperic996@gmail.com>
2023-11-09 16:31:50 +0100

    zadatak 2

---
+++ sidebar.php
@@ -0,0 +1,29 @@
+
+<aside class="col-sm-3 ml-sm-auto blog-sidebar">
+    <div class="sidebar-module sidebar-module-inset">
+        <h4>About</h4>
+        <p>Nesto nesto bla <em>nesto vazno </em> Malo random teksta. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    </div>
+    <div class="sidebar-module">
+        <h4>Archives</h4>
+        <ol class="list-unstyled">
+            <li><a href="#">March 2014</a></li>
+            <li><a href="#">February 2014</a></li>
+            <li><a href="#">January 2014</a></li>
+            <li><a href="#">December 2013</a></li>
+            <li><a href="#">November 2013</a></li>
+        </ol>
+    </div>
+    <div class="sidebar-module">
+        <h4>Elsewhere</h4>
+        <ol class="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">LinkedIn</a></li>
+            
+        </ol>
+    </div>
+</aside>
+
+
