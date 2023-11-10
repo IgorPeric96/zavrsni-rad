125f285714534830b624d61f828f8e35c292b21f
IgorPeric96 <igorperic996@gmail.com>
2023-11-10 01:14:52 +0100

    zadatak 7

--- sidebar.php
+++ sidebar.php
@@ -1,27 +1,19 @@
+<?php
+$sql4 = "SELECT * FROM posts ORDER BY created_at DESC LIMIT 3";
+$posts5 = getData($sql4, $connection, $fetchAll = true);
+?>
 
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
     <div class="sidebar-module sidebar-module-inset">
-        <h4>About</h4>
-        <p>Nesto nesto bla <em>nesto vazno </em> Malo random teksta. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-    </div>
-    <div class="sidebar-module">
-        <h4>Archives</h4>
+        <h4>Latest posts</h4>
         <ol class="list-unstyled">
-            <li><a href="#">March 2014</a></li>
-            <li><a href="#">February 2014</a></li>
-            <li><a href="#">January 2014</a></li>
-            <li><a href="#">December 2013</a></li>
-            <li><a href="#">November 2013</a></li>
-        </ol>
-    </div>
-    <div class="sidebar-module">
-        <h4>Elsewhere</h4>
-        <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">LinkedIn</a></li>
-            
+        <?php foreach ($posts5 as $post) { ?>
+                <li>
+                    <a href="single-post.php?post_id=<?php echo ($post['id']) ?>">
+                        <?= $post['title'] ?>
+                    </a>
+                </li>
+            <?php } ?>          
         </ol>
     </div>
 </aside>
